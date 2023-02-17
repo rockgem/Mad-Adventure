558ead7fa90be407950466f9dd724b89b4339e14
rockgem <63050183+rockgem@users.noreply.github.com>
2023-02-17 08:17:32 +0800

    add tiles collisions

--- actors/Player.gd
+++ actors/Player.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 
 var vel = Vector2.ZERO
 var move_speed = 900.0
-var gravity = 2000.0
+var gravity = 4000.0
 
 
 
@@ -16,7 +16,7 @@ func _physics_process(delta):
 	vel.x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
 	
 	if Input.is_action_just_pressed('ui_up') and is_on_floor():
-		vel.y = -1000.0
+		vel.y = -2000.0
 	
 	
 	vel = move_and_slide(Vector2(vel.x * move_speed, vel.y), Vector2.UP)
