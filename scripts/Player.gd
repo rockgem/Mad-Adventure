87b8383795a8543a9bbbaedbb1da60c730425666
rockgem <63050183+rockgem@users.noreply.github.com>
2023-02-19 09:42:40 +0800

    player anims, npc and enemy init

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,9 +3,9 @@ extends KinematicBody2D
 
 
 var vel = Vector2.ZERO
-var move_speed = 900.0
-var gravity = 4000.0
-
+var move_speed = 100.0
+var gravity = 900.0
+var dir = 1
 
 
 
@@ -15,8 +15,25 @@ func _physics_process(delta):
 	
 	vel.x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
 	
+	dir = vel.x
+	
+	if dir < -0.5:
+		$AnimatedSprite.flip_h = true
+	if dir > 0.5:
+		$AnimatedSprite.flip_h = false
+	
+	
+	if vel != Vector2.ZERO:
+		if is_on_floor():
+			$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
+	
+	
 	if Input.is_action_just_pressed('ui_up') and is_on_floor():
-		vel.y = -2000.0
+		$AnimatedSprite.frame = 0
+		$AnimatedSprite.play("jump")
+		vel.y = -400.0
 	
 	
 	vel = move_and_slide(Vector2(vel.x * move_speed, vel.y), Vector2.UP)
