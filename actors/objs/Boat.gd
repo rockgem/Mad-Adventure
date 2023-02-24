9edcfffb0c7357d93b02cce49d14458d0fb2c0a5
rockgem <63050183+rockgem@users.noreply.github.com>
2023-02-24 23:28:00 +0800

    add map navigation, limit map scroll

---
+++ actors/objs/Boat.gd
@@ -0,0 +1,7 @@
+extends Sprite
+
+
+
+func _on_BoatArea_input_event(viewport, event, shape_idx):
+	if InputEventScreenTouch and !event.pressed:
+		pass
---
+++ actors/ui/MapNavigation.gd
@@ -0,0 +1,23 @@
+extends Control
+
+onready var limit_right = $Panel/Panel/Map.rect_size.x - $Panel/Panel.rect_size.x
+onready var limit_down =  $Panel/Panel/Map.rect_size.y - $Panel/Panel.rect_size.y
+
+func _ready():
+	print(-limit_right)
+	print(-limit_down)
+
+
+func _on_Map_gui_input(event):
+	if event is InputEventScreenDrag:
+		get_node('%Map').rect_position += event.relative
+		
+		if get_node('%Map').rect_position.x > 0.0:
+			get_node('%Map').rect_position.x = 0.0
+		if get_node('%Map').rect_position.x < -limit_right:
+			get_node('%Map').rect_position.x = -limit_right
+		
+		if get_node('%Map').rect_position.y > 0.0:
+			get_node('%Map').rect_position.y = 0.0
+		if get_node('%Map').rect_position.y < -limit_down:
+			get_node('%Map').rect_position.y = -limit_down
