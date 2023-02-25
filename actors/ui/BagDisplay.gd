d497892ac0a0b3331ae9a8cc09ce921b33491aac
rockgem <63050183+rockgem@users.noreply.github.com>
2023-02-25 18:36:39 +0800

    displaying inv item stack

--- actors/ui/BagDisplay.gd
+++ actors/ui/BagDisplay.gd
@@ -11,9 +11,14 @@ func display_item(data: Dictionary):
 	item = Item.new()
 	item.props = data.duplicate()
 	
+	if item.props['stack'] > 1:
+		$Amount.text = 'x' + str(item.props['stack'])
+		$Amount.show()
+	
 	$Icon.texture = load("res://assets/items/%s.png" % item.props['id'])
 
 
 func remove_item():
 	$Icon.texture = null
 	item = null
+	$Amount.hide()
