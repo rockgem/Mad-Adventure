2f672e1ad0678b6586bd4f1a26755bee6624fcd9
rockgem <63050183+rockgem@users.noreply.github.com>
2023-02-28 12:43:08 +0800

    add styles, coin minus anim

--- actors/ui/Shop.gd
+++ actors/ui/Shop.gd
@@ -30,3 +30,12 @@ func _on_Buy_pressed():
 	
 	ManagerGame.player_data['gold'] -= current_selected_item.props['price']
 	get_node('%Gold').get_node("Value").text = str(ManagerGame.player_data['gold'])
+	
+	get_node('%Floater').show()
+	get_node('%Floater').text = '-%s' % current_selected_item.props['price']
+	var t = get_tree().create_tween()
+	t.tween_property(get_node('%Floater'), 'rect_position:y', -8.0, 0.4)
+	yield(t, "finished")
+	get_node('%Floater').rect_position.y = 0
+	get_node('%Floater').hide()
+	
