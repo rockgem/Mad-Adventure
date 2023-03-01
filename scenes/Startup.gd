b51d5987c9b6825e6fed4bea72f63613cc44981f
rockgem <63050183+rockgem@users.noreply.github.com>
2023-03-01 19:39:26 +0800

    login with email and password

--- scenes/Startup.gd
+++ scenes/Startup.gd
@@ -4,8 +4,10 @@ extends Control
 
 func _ready():
 	Firebase.Auth.connect("auth_request", self, 'on_auth_request')
+	Firebase.Auth.connect("request_completed", self, 'on_request_completed')
 	Firebase.Auth.connect("login_succeeded", self, 'on_login_succeeded')
 	Firebase.Auth.connect("signup_succeeded", self, 'on_signup_succeeded')
+	Firebase.Auth.connect("signup_failed", self, 'on_signup_failed')
 	Firebase.Auth.connect("userdata_received", self, 'on_userdata_received')
 	Firebase.Auth.connect("token_refresh_succeeded", self, 'on_token_refresh_succeeded')
 	
@@ -27,6 +29,11 @@ func on_signup_succeeded(auth_result):
 	ManagerGame.new_game()
 
 
+func on_signup_failed(code, message):
+	get_node('%EmailSignupPanel').get_node("VBoxContainer/Error").text = message
+	get_node('%EmailSignupPanel').get_node("VBoxContainer/Error").show()
+
+
 func on_login_succeeded(auth_result):
 	login_to_game()
 	ManagerGame.load_game()
@@ -37,6 +44,33 @@ func on_token_refresh_succeeded(auth_result):
 	ManagerGame.load_game()
 
 
+func on_request_completed():
+	pass
+
+
+
+
 func _on_Anon_pressed():
 	$SignupControl.hide()
 	Firebase.Auth.login_anonymous()
+
+
+func _on_Email_pressed():
+	get_node('%EmailSignupPanel').show()
+
+
+func _on_SignupEmail_pressed():
+	var e = get_node('%EmailField').text
+	var p = get_node('%PassField').text
+	Firebase.Auth.signup_with_email_and_password(e, p)
+
+
+func _on_Cancel_pressed():
+	get_node('%EmailSignupPanel').hide()
+
+
+
+
+
+
+
